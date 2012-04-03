66fd2d1209684503524bade644a59edb4aec4529
Kees Huiberts <itissohardtothinkofagoodemail@gmail.com>
2012-04-03 17:52:32 +0200

    actually, debug.sh should not be included in the revisioning

--- debug.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rf bin &&
-ant debug &&
-adb install -r bin/Integral-debug.apk &&
-adb shell am start -a android.intent.action.main -n org.beide.integral/org.beide.integral.Integral
